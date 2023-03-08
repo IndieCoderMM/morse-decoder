3c8bab2edb1ce4420bfa5b1ec43e57d5fa327582
heinthant <hthant00chk@gmail.com>
2023-03-08 21:53:58 +0630

    Add word decoder

--- decoder.rb
+++ decoder.rb
@@ -4,11 +4,23 @@ MORSE_TABLE = {'.-' => 'a', '-...' => 'b',
                '..' => 'i', '.---' => 'j', '-.-' => 'k',
                '.-..' => 'l', '--' => 'm', '-.' => 'n', '---' => 'o',
                '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's',
-               '-' => 't', '..-' => 'u', '...-' => 'v', '.--': 'w', '-..-': 'x',
+               '-' => 't', '..-' => 'u', '...-' => 'v', '.--'=> 'w', '-..-'=> 'x',
                '-.--' => 'y', '--..' => 'z'}.freeze
 
 def decode_char(char)
   MORSE_TABLE[char]
 end
 
-puts decode_char('.-')
+def decode_word(word)
+    letters = word.split
+    decoded= ''
+    letters.each {|c| decoded += decode_char(c)}
+    return decoded
+end
+
+def decode(message)
+    # TODO Method to decode entire message
+end
+
+# puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
+
