566654f191209256b2bc8f1af5b7c542653c3f3f
heinthant <hthant00chk@gmail.com>
2023-03-08 22:48:13 +0630

    Fix linter errors

--- decoder.rb
+++ decoder.rb
@@ -1,26 +1,25 @@
-MORSE_TABLE = {'.-' => 'a', '-...' => 'b',
-               '-.-.' => 'c', '-..' => 'd', '.' => 'e',
-               '..-.' => 'f', '--.' => 'g', '....' => 'h',
-               '..' => 'i', '.---' => 'j', '-.-' => 'k',
-               '.-..' => 'l', '--' => 'm', '-.' => 'n', '---' => 'o',
-               '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's',
-               '-' => 't', '..-' => 'u', '...-' => 'v', '.--'=> 'w', '-..-'=> 'x',
-               '-.--' => 'y', '--..' => 'z'}.freeze
+MORSE_TABLE = { '.-' => 'a', '-...' => 'b',
+                '-.-.' => 'c', '-..' => 'd', '.' => 'e',
+                '..-.' => 'f', '--.' => 'g', '....' => 'h',
+                '..' => 'i', '.---' => 'j', '-.-' => 'k',
+                '.-..' => 'l', '--' => 'm', '-.' => 'n', '---' => 'o',
+                '.--.' => 'p', '--.-' => 'q', '.-.' => 'r', '...' => 's',
+                '-' => 't', '..-' => 'u', '...-' => 'v', '.--' => 'w', '-..-' => 'x',
+                '-.--' => 'y', '--..' => 'z' }.freeze
 
 def decode_char(char)
   MORSE_TABLE[char]
 end
 
 def decode_word(word)
-    letters = word.split
-    decoded= ''
-    letters.each {|c| decoded += decode_char(c)}
-    return decoded
+  letters = word.split
+  decoded = ''
+  letters.each { |c| decoded += decode_char(c) }
+  decoded
 end
 
 def decode(message)
-    # TODO Method to decode entire message
+  # TODO: Method to decode entire message
 end
 
 # puts decode(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...")
-
